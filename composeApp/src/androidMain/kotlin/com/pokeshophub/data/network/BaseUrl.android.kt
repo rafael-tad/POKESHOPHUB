b15a5ae4deb6302ee95cad6f3384fe010f8e38f0
rafael-tad <raaffa.tad@gmail.com>
2026-06-21 12:55:02 +0200

    PokeShop Hub

--- composeApp/src/androidMain/kotlin/com/pokeshophub/data/network/BaseUrl.android.kt
+++ composeApp/src/androidMain/kotlin/com/pokeshophub/data/network/BaseUrl.android.kt
@@ -7,8 +7,8 @@ import android.os.Build
  * - Cambia IS_PRODUCTION a true para que la app móvil se conecte al servidor en la nube.
  * - Coloca la URL de tu servicio web de Render en PRODUCTION_URL (ej: "https://pokeshop-backend.onrender.com").
  */
-private const val IS_PRODUCTION = false
-private const val PRODUCTION_URL = "https://pokeshop-backend.onrender.com" 
+private const val IS_PRODUCTION = true
+private const val PRODUCTION_URL = "https://pokeshophub-backend.onrender.com"
 
 actual fun getBaseUrl(): String {
     return if (IS_PRODUCTION) PRODUCTION_URL else "http://127.0.0.1:8080"
